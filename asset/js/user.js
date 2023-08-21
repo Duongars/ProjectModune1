c4c44871dfad2fff4b1597efd43b91add4a07e92
Duongars <133123635+Duongars@users.noreply.github.com>
2023-08-21 13:04:18 +0700

    Add files via upload

---
+++ asset/js/user.js
@@ -0,0 +1,16 @@
+let userBase = [
+    { id : "Bùi Nguyễn Dương",
+        email : "abc@gmail.com"
+     },
+     { id : "Nguyễn Văn A",
+     email : "def@gmail.com"
+     },
+     { id : "Trần Văn B",
+     email : "ghj@gmail.com"
+     }
+
+]
+
+  
+
+
